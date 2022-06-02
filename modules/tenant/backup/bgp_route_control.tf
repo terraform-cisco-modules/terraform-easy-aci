8f5750205df3d19c3583268cc76a38f4be9f888f
scotttyso <tyscott@cisco.com>
2022-06-02 01:22:07 -0400

    removing duplicate entry

--- modules/tenant/backup/bgp_route_control.tf
+++ modules/tenant/backup/bgp_route_control.tf
@@ -3,15 +3,6 @@
 # 
 # Example Usage
 resource "aci_bgp_route_control_profile" "example" {
-  parent_dn                  = aci_tenant.tenentcheck.id
-  name                       = "one"
-  annotation                 = "bgp_route_control_profile_tag"
-  description                = "from terraform"
-  name_alias                 = "example"
-  route_control_profile_type = "global"
-}
-
-resource "aci_bgp_route_control_profile" "example" {
   parent_dn                  = aci_l3_outside.example.id
   name                       = "bgp_route_control_profile_1"
   annotation                 = "bgp_route_control_profile_tag"
