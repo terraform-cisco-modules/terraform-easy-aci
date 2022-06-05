091024a4b287beb15369157887fe36216b795326
scotttyso <tyscott@cisco.com>
2022-06-05 09:42:47 -0400

    removed vpc_policy dependency

--- modules/switch/vpc_domains.tf
+++ modules/switch/vpc_domains.tf
@@ -41,13 +41,12 @@ GUI Location:
 resource "aci_vpc_explicit_protection_group" "vpc_domains" {
   depends_on = [
     aci_rest_managed.fabric_membership,
-    aci_vpc_domain_policy.vpc_domain_policies
   ]
   for_each                         = { for k, v in local.vpc_domains : k => v if length(v.switches) > 1 }
   annotation                       = each.value.annotation != "" ? each.value.annotation : var.annotation
   name                             = each.key
   switch1                          = element(each.value.switches, 0)
   switch2                          = element(each.value.switches, 1)
-  vpc_domain_policy                = each.value.vpc_domain_policy
+  vpc_domain_policy                = "uni/fabric/vpcInst-${each.value.vpc_domain_policy}"
   vpc_explicit_protection_group_id = each.value.domain_id
 }
